572e2d6702be09129b68f162cca289a118f11914
Prajwal Lohan <prajwal.lohan@yahoo.com>
2022-03-06 01:44:44 +0100

    split tree function

--- decision_tree/DecisionTreeRegressor.py
+++ decision_tree/DecisionTreeRegressor.py
@@ -9,9 +9,10 @@ class DecisionTreeRegressor():
                  min_samples_leaf=None,
                  random_state=None,
                  max_leaf_nodes=None,
-                 min_impurity_decrease=None,
-                 min_impurity_split=None):
+                 min_impurity_decrease=None):
 
+        self.split_feature = None
+        self.split_value = None
         self.n_samples = None
         self.impurity_decrease = None
         self.left_child = None
@@ -21,13 +22,20 @@ class DecisionTreeRegressor():
         self.criterion = criterion
         self.max_depth = max_depth
 
+        self.min_samples_split = min_samples_split
+        self.min_samples_leaf = min_samples_leaf
+        self.random_state = random_state
+        self.max_leaf_nodes = max_leaf_nodes
+        self.min_impurity_decrease = min_impurity_decrease
+
     def fit(self, X, y):
         # initialise root node
-        self.root = DecisionTreeRegressor()
+        self.root = DecisionTreeRegressor(criterion=self.criterion,
+                                          max_depth=self.max_depth,
+                                          min_impurity_decrease=self.min_impurity_decrease)
 
         # grow the tree recursively starting from root node
-        self.root.grow_tree(self, X, y)
-        return
+        self.root.grow_tree(X, y)
 
     def predict(self, X):
 
@@ -35,26 +43,64 @@ class DecisionTreeRegressor():
 
     def grow_tree(self, X, y):
 
-        # find best split
-        self.best_split(self,X,y)
+        # Stop growing tree according to stopping conditions
+        if self.depth >= self.max_depth:
+            return
 
+        self.n_samples = X.shape[0]
+
+        # find best split
+        self.best_split(X, y)
+
+        # todo: Question: if for best split, min_samples_leaf is not satisfied for one child,
+        #  but it is satisfied for another case of split (not max impurity decrease), what to do?
+        #  can this situation ever occur?
+
+        if self.split_feature is None:
+            return
+
+        # todo: check impurity decrease condition
 
         # split the tree using this best split
-        self.split_tree(self, X, y)
+        self.split_tree(X, y)
 
         return
 
-    def split_tree(self):
+    def split_tree(self, X, y):
 
-        return
+        # Split X and y according to split_value
+        left_indices = [X[:, self.split_feature <= self.split_value]]
+        right_indices = [X[:, self.split_feature > self.split_value]]
 
-    def best_split(self):
+        X_left = X[left_indices]
+        y_left = y[left_indices]
+
+        X_right = X[right_indices]
+        y_right = y[right_indices]
+
+        # Create left child and grow it recursively
+        self.left_child = DecisionTreeRegressor(criterion=self.criterion,
+                                                max_depth=self.max_depth,
+                                                min_impurity_decrease=self.min_impurity_decrease)
+        self.left_child.depth = self.depth + 1
+        self.left_child.grow_tree(X_left, y_left)
+
+        # Create right child and grow it recursively
+        self.right_child = DecisionTreeRegressor(criterion=self.criterion,
+                                                 max_depth=self.max_depth,
+                                                 min_impurity_decrease=self.min_impurity_decrease)
+        self.right_child.depth = self.depth + 1
+        self.right_child.grow_tree(X_right, y_right)
+
+    def best_split(self, X, y):
 
         return
 
     def node_impurity(self, criterion, y):
+
         if criterion == 'mse':
             return np.mean((y - np.mean(y)) ** 2)
+
         elif criterion == 'mae':
-            return
+            return np.mean(y - np.mean(y))  # todo: is this correct?
         return
--- decision_tree/tester.py
+++ decision_tree/tester.py
@@ -1,23 +1,51 @@
+from decision_tree.DecisionTreeRegressor import DecisionTreeRegressor
 from sklearn import tree
+from sklearn.model_selection import train_test_split
 import numpy as np
 from sklearn.datasets import make_moons
 import matplotlib.pyplot as plt
 
-# reg = my decision tree regressor
-skreg = tree.DecisionTreeRegressor()
-
+# if __name__=="__main__":
 # # Classification dataset
-N = 1000
-X, Y = make_moons(N, noise=0.2)
+# N = 1000
+# X, Y = make_moons(N, noise=0.2)
 # plt.scatter(X[:, 0], X[:, 1], c=Y)
 # plt.show()
 
 # Regression dataset
 N = 200
 X = np.linspace(-1, 1, N)
-Y = X ** 2 + np.random.normal(0, 0.07, N)
+y = X ** 2  # + np.random.normal(0, 0.07, N)
 X = X.reshape(-1, 1)
-plt.scatter(X, Y)
-plt.show()
+# plt.scatter(X, y,c="b")
+# plt.show()
 
 # fit X and y and then predict with sklearn dt and my dt to compare
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+
+reg = DecisionTreeRegressor(criterion='mse', max_depth=4)
+skreg = tree.DecisionTreeRegressor(criterion='mse', max_depth=4)
+
+reg.fit(X, y)
+skreg.fit(X, y)
+
+# reg.fit(X_train, y_train)
+# skreg.fit(X_train, y_train)
+
+my_pred = reg.predict(X)
+sk_pred = skreg.predict(X)
+
+# my_pred = reg.predict(X_test)
+# sk_pred = skreg.predict(X_test)
+
+
+print("My dt result: ", my_pred)
+print("Sklearn dt result: ", sk_pred)
+
+# plt.scatter(X_train, y_train)
+# plt.scatter(X_test, y_test)
+
+plt.scatter(X, y, c="b", linewidths=0.1)
+plt.plot(X, sk_pred, c="g")
+plt.plot(X, my_pred, c="r")
