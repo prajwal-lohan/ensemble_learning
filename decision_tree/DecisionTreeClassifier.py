7c6d1e2e5bf04758a1be7a52f36e6cdeecd77ef7
hannah-hole <76449733+hannah-hole@users.noreply.github.com>
2022-03-10 21:45:01 +0100

    Updates
    Updates including predict and build tree and accuracy functions

--- decision_tree/DecisionTreeClassifier.py
+++ decision_tree/DecisionTreeClassifier.py
@@ -11,11 +11,17 @@ X,Y=make_moons(N, noise=0.2)
 plt.scatter(X[:,0], X[:,1],c=Y)
 plt.show()
 
-def calculate_gini(X):
-  classes= np.unique(Y) #first we get the number of classes from our Y
+#calc gini for the node  
+def calculate_gini(node):
+  classes=[] #create a list for the classes
+  for row in node:
+    classes.append((row[-1])) #we create a list of classes (Y)
+  
+  list_of_classes= np.unique(classes) #first we get the number of classes from our Y
   n_instances=[] #create a list that will store the number of each class for that node
-  for cls in classes: #for each class
-    n_instance_class=sum(X==cls) #calculate the total number of instances of that class in the node
+  for cls in list_of_classes: #for each class
+    
+    n_instance_class=sum(classes==cls) #calculate the total number of instances of that class in the node
     n_instances.append(n_instance_class)
   
   #now calculate the gini for that node
@@ -27,61 +33,98 @@ def calculate_gini(X):
     gini_node-=(n/node_size)**2
   return gini_node
   
-  n_features=X.shape[1] #get number of variables from dataset
-d=dict(zip(list(range(n_features)), sorted(zip(*X)))) #for each feature/var for the full dataset we add all values to a dictionary. 
+  n_features=X.shape[1]
 
-def get_split(X, Y):
-  best_gini=1 #initiate best gini value
-  best_feature, best_split=None, None
-  for k in X.keys(): #for each key (feature/variable)
-    feature_number=list(X)[k] #which variable/feature number we are splitting by e.g first of two vars
-    for j in range(len(Y)): #for each value in dictionary key 
-      current_split_value=X[k][j] #we split by this value
-      left_node_class, right_node_class=[],[] #create empty lists for the classes of left and right nodes 
-      left_node_x, right_node_x= collections.defaultdict(list),collections.defaultdict(list) #create empty lists for the left and right nodes variable values 
-      for i in range(len(Y)):  #iterate over each value in the dataset
-        if d[k][i]<=current_split_value: #if current datapoint is less than split value it goes into left node, else right 
-          left_node_class.append(Y[i])
-          left_node_x[feature_number].append(X[feature_number][i]) #we append it and need to append the corresponding other x values 
-          left_node_x[0].append(X[0][i]) #****
+#get split
+def get_split(node): 
+  best_gini,best_feature, best_split=1, None, None #initiate best gini value
+
+  for k in range(n_features): #for each key (feature/variable)
+    #feature_number=list(X)[k] #which variable/feature number we are splitting by e.g first of two vars
+    for j in range(len(node)): #for each value in dictionary key 
+      current_split_value=node[j][k] #we split by this value
+      left, right=list(),list() #create empty lists for the classes of left and right nodes 
+      for i in range(len(node)):  #iterate over each value in the dataset
+        if node[i][k]<=current_split_value: #if current datapoint is less than split value it goes into left node, else right . HH TO DOUBLE CHECK IF <= OR<
+          left.append(node[i])
         else:
-          right_node_class.append(Y[i]) 
-          right_node_x[feature_number].append(X[feature_number][i]) #we append it and need to append the corresponding other x values **
-          right_node_x[0].append(X[0][i]) 
+          right.append(node[i])
 
-      global best_node_left, best_node_left_x, best_node_right, best_node_right_x
       #calculate gini at current split
-      region_size=len(left_node_class)+len(right_node_class) #first get number of values in each node
-      weighted_gini=len(left_node_class)/(region_size)*calculate_gini(left_node_class) +len(right_node_class)/(region_size)*calculate_gini(right_node_class)  #calculate the weighted gini
-      #print('X',feature_number,  current_split_value, 'Gini=', weighted_gini)
+      region_size=len(left)+len(right) #first get number of values in each node
+      weighted_gini=len(left)/(region_size)*calculate_gini(left) +len(right)/(region_size)*calculate_gini(right)  #calculate the weighted gini
+
       if weighted_gini<best_gini: #if current gini calc is better than best gini so far:
-        best_gini, best_feature, best_split, best_node_left,best_node_right, best_node_left_x,best_node_right_x =weighted_gini, feature_number, current_split_value, left_node_class, right_node_class, right_node_x #set new best values
-#print the split
-  print("Best split is X",best_feature, "<", best_split, "Gini=", best_gini )
+        best_gini, best_feature, best_split, best_left,best_right=weighted_gini, k, current_split_value, left, right#set new best values
+  return {'index':best_feature, 'value': best_split, 'left':best_left, 'right':best_right, 'gini':best_gini} #STORE node information in a dictionary
 
 def final_label(node):
-  node_dict=dict((i, node.count(i)) for i in node) #create a dictionary with a count of each class 
+  classes=[]
+  for row in node:
+    classes.append((row[-1])) #we create a list of classes (Y)
+  node_dict=dict((i, classes.count(i)) for i in classes) #create a dictionary with a count of each class 
   label=max(node_dict, key=node_dict.get) #take class with maximum number of counts
-  print("label:", label)
+  print(label, ":",sum(node_dict.values()),node_dict)
   return label
 
-def recursive_split(X, max_depth, depth):
-  if len(best_node_right)==0: #if a left or right node is empty or we are at a depth larger than the max_depth of the tree, it is a terminal node so assign label to it 
-    right_child_node=final_label(best_node_right)
-    #print("right node terminates")
+def recursive_split(node, max_depth, depth, min_size):
+  left, right=node['left'], node['right']
+  del node['left'], node['right']
+  
+  if not left or not right or node['gini']==0:
+    node['left']=node['right']=final_label(left+right)
     return
-  if len(best_node_left)==0:
-    left_child_node=final_label(best_node_left)
-    return
+
+  #check if we have exceeded the maximum epth
   if depth>=max_depth:
-    final_label(best_node_right)
-    final_label(best_node_left)
+    node['left'], node['right']=final_label(left), final_label(right)
     return
+
+    #checking if we are below the minimum size
+  if len(left)<=min_size:
+    node['left']=final_label(left)
   else:
-    left_child_node=get_split(best_node_left_x, best_node_left) #else we will continue splitting and building out the tree
-    recursive_split(left_child_node, max_depth, depth+1) 
-    right_child_node=get_split(best_node_right_x, best_node_right) 
-    recursive_split(right_child_node, max_depth, depth+1) 
+    node['left']=get_split(left)
+    recursive_split(node['left'], max_depth, depth+1, min_size) 
+    
+  if len(right)<=min_size:
+    node['right']=final_label(right)
+
+  else:
+    node['right']=get_split(right) 
+    recursive_split(node['right'], max_depth, depth+1, min_size)     
+    
+   def build_tree(node, max_depth, min_size):
+  root=get_split(dataset) #here we start with the initial full dataset
+  recursive_split(root, max_depth, min_size, 1) # it will keep calling this function (as input into next run).d, Y, max_depth, depth)
+  return root
+
+def predict(node, row): 
+	if row[node['index']] < node['value']:
+		if type(node['left'])==dict:
+			return predict(node['left'], row) #keep iterating
+		else:
+			return node['left']
+	else:
+		if type(node['right'])==dict:
+			return predict(node['right'], row)
+		else:
+			return node['right']
+
+tree=build_tree(dataset, 1, 1) 
+tree
+predictions=list()
+for row in dataset: 
+  prediction=predict(tree, row)
+  predictions.append(prediction)
   
-  def predict():
-  
+ def accuracy(predictions, Y):
+  correct=0
+  for i in range(len(Y)):
+    if predictions[i]==Y[i]:
+      correct+=1
+
+  accuracy=correct/len(Y)
+  print(accuracy)
+
+accuracy(predictions, Y) 
